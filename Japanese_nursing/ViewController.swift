349241e50286bfd9b1b208668d381432180cfc91
yui-yoshizawa <yui.yossy.0114@gmail.com>
2020-06-03 21:48:43 +0900

    first commit

---
+++ Japanese_nursing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Japanese_nursing
+//
+//  Created by 吉澤優衣 on 2020/06/03.
+//  Copyright © 2020 吉澤優衣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
