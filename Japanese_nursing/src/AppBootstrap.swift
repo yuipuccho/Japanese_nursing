7559fca8d5538ece599d9948f37e50b28d7a3ca2
yui-yoshizawa <yui.yossy.0114@gmail.com>
2020-06-27 14:29:23 +0900

    [Add] tab

---
+++ Japanese_nursing/src/AppBootstrap.swift
@@ -0,0 +1,9 @@
+//
+//  AppBootstrap.swift
+//  Japanese_nursing
+//
+//  Created by 吉澤優衣 on 2020/06/27.
+//  Copyright © 2020 吉澤優衣. All rights reserved.
+//
+
+import Foundation
---
+++ Japanese_nursing/src/Feature/TabViews/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  Japanese_nursing
+//
+//  Created by 吉澤優衣 on 2020/06/27.
+//  Copyright © 2020 吉澤優衣. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
