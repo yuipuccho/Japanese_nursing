d82ae5de217b7d5fe219aacdc6e51daf50f2de75
yui-yoshizawa <yui.yossy.0114@gmail.com>
2020-07-30 16:24:17 +0900

    [Add] StudyTabContainerViewModelProtocol

--- Japanese_nursing/src/viewModels/Study/StudyTabContainerViewModel.swift
+++ Japanese_nursing/src/viewModels/Study/StudyTabContainerViewModel.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2020 吉澤優衣. All rights reserved.
 //
 
-import Foundation
+import Tabman
+import UIKit
+
+/**
+ * 学習タブ画面ViewModel対応Protocol
+ */
+protocol StudyTabContainerViewModelProtocol {
+
+    /// タブ内のViewController
+    var viewControllers: [UIViewController] { get }
+
+    /// タブ表示文字列
+    var items: [TMBarItem] { get }
+
+}
