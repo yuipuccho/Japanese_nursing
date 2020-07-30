1e748a22987fccd28c54042c4cd67b9986020761
yui-yoshizawa <yui.yossy.0114@gmail.com>
2020-07-30 21:57:25 +0900

    [Add] Firebase to project

--- Japanese_nursing/src/AppDelegate.swift
+++ Japanese_nursing/src/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 // MARK: - AppDelegate
 
@@ -35,6 +36,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         sleep(2)
+
+        // Firebase
+        FirebaseApp.configure()
 
         //AppBootstrap.boost()
 
