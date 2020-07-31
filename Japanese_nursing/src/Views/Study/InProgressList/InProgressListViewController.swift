8bc934be1ee273936fd33b1c8fb946305dcda36e
yui-yoshizawa <yui.yossy.0114@gmail.com>
2020-07-31 15:31:43 +0900

    [Add] Display Cell

--- Japanese_nursing/src/Views/Study/InProgressList/InProgressListViewController.swift
+++ Japanese_nursing/src/Views/Study/InProgressList/InProgressListViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class InProgressListViewController: UITableViewController {
 
-    var list = ["aaa", "bbb"]
-
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.register(UINib(nibName: "StudyListView", bundle: nil), forCellReuseIdentifier: "customCell")
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! StudyListView
         return cell
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 82
     }
 
 //    // MARK: - TableViewDataSource
