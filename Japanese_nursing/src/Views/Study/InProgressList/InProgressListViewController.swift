fdd26ab819c8794a0072e6810a6a825c80d89ad4
yui-yoshizawa <yui.yossy.0114@gmail.com>
2020-07-31 15:03:09 +0900

    [Add] Xib

--- Japanese_nursing/src/Views/Study/InProgressList/InProgressListViewController.swift
+++ Japanese_nursing/src/Views/Study/InProgressList/InProgressListViewController.swift
@@ -8,24 +8,27 @@
 
 import UIKit
 
-class InProgressListViewController: UIViewController {
+class InProgressListViewController: UITableViewController {
+
+    var list = ["aaa", "bbb"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.tableView.register(UINib(nibName: "StudyListView", bundle: nil), forCellReuseIdentifier: "customCell")
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! StudyListView
+        return cell
     }
-    */
+
+//    // MARK: - TableViewDataSource
+//    /// DataSource のセットアップ
+//    private func setupDataSource() {
+//        //let dataSource =
+//    }
+
+
 
 }
 
---
+++ Japanese_nursing/src/Views/Study/ViewForCell/StudyListView.swift
@@ -0,0 +1,17 @@
+//
+//  StudyListView.swift
+//  Japanese_nursing
+//
+//  Created by 吉澤優衣 on 2020/07/31.
+//  Copyright © 2020 吉澤優衣. All rights reserved.
+//
+
+import UIKit
+
+class StudyListView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
