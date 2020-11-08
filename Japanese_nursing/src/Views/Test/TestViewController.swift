8829ff22f228b2d6dd7cfd4742dc97a92f65538b
yui-yoshizawa <yui.yossy.0114@gmail.com>
2020-11-08 15:39:07 +0900

    [Add] Layout

---
+++ Japanese_nursing/src/Views/Test/TestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TestViewController.swift
+//  Japanese_nursing
+//
+//  Created by 吉澤優衣 on 2020/11/08.
+//  Copyright © 2020 吉澤優衣. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
