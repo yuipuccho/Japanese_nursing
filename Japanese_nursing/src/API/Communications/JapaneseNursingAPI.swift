368604ed51b863b10111ab260983c3f96e9dc130
yui-yoshizawa <yui.yossy.0114@gmail.com>
2020-12-03 13:41:10 +0900

    setting

--- Japanese_nursing/src/API/Communications/JapaneseNursingAPI.swift
+++ Japanese_nursing/src/API/Communications/JapaneseNursingAPI.swift
@@ -69,8 +69,7 @@ public struct API {
             return "https"
         } else {
             // TODO: 後で追記
-            return ""
-        }
+            return "https"        }
     }()
 
     /// 接続先ホスト指定
@@ -80,8 +79,7 @@ public struct API {
         if Constants.DEBUG {
             return "damp-dusk-43056.herokuapp.com"
         } else {
-            // TODO: 後で追記
-            return ""
+            return "damp-dusk-43056.herokuapp.com"
         }
     }()
 
